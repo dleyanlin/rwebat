dd477ed599c1886e87828e93e4fb86eb75142a5b
dleyanlin <lindleyan@hotmail.com>
2014-05-10 16:38:22 +0800

    Delete webex.rb

--- examples/features/pages/webex.rb
+++
@@ -1,6 +0,0 @@
-
-module Webex
-  access_to(:mc) { frame(:name,"header").td(:id,"itemTDMC")}
-  access_to(:claendar) { frame(:name,"mainFrame").frame(:name,"main").img(:alt,"Calendar")}
-  access_to(:joinnow) { button(:name,"JoinNow") }
-end
