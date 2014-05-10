411e73bed6c2eeedc4200f0722638832d71a2faa
dleyanlin <lindleyan@hotmail.com>
2014-05-10 16:40:06 +0800

    Update env.rb

--- examples/features/support/env.rb
+++ examples/features/support/env.rb
@@ -1,7 +1,5 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../../', 'lib'))
 require 'rwebat'
-require File.expand_path(File.dirname(__FILE__)+"/../")+'/pages/webex'
-
 
 include Rwebat
-include Webex
+
